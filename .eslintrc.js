bedf21895b4b93dc3b68ae765881c98a8584fcc3
Aaron Friedman <afriedman111@hotmail.com>
2023-11-18 15:11:59 -0500

    fix: added eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+      'prettier'
+    ],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/eslint-recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    rules: {
+      'prettier/prettier': 'error',
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-prototype-builtins": "off"
+    }
+  };
