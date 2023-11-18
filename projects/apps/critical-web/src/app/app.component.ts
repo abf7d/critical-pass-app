5d97b9e9da66c55696aa52b2362021b091d898cb
Aaron Friedman <afriedman111@hotmail.com>
2023-11-18 16:10:40 -0500

    fix: lint staged test

--- projects/apps/critical-web/src/app/app.component.ts
+++ projects/apps/critical-web/src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
         this.eventService.get('test').subscribe(data => {
             console.log(data);
         });
-        var test2 = 0;
+        var test2 = 'asdfsadfsdf asdf asdf sadf ads afd sasdf sd fdas ad dsa sfd asd fa dsd af df ds dsa asd ds af sd asd fsd fsd fsad fds ';
         var test = 0;
     }
 }
