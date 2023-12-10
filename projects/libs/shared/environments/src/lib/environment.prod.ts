f586d1b25e49192b15c4143adcef4ca0d46dc80a
Aaron Friedman <afriedman111@hotmail.com>
2023-12-10 17:14:42 -0500

    fix: uncommenting web config, updating env redirect and logout url

--- projects/libs/shared/environments/src/lib/environment.prod.ts
+++ projects/libs/shared/environments/src/lib/environment.prod.ts
@@ -6,11 +6,11 @@ export const environment = {
     mappingApi: 'https://criticalpathapi.azurewebsites.net/api/', // for msproj files and building arrow diagram
     authority: 'https://criticalplayground.b2clogin.com/criticalplayground.onmicrosoft.com/B2C_1_DefaultSignInSignUp',
     knownAuthorities: ['criticalplayground.b2clogin.com'],
-    redirectUri: 'https://criticalpass.azurewebsites.net/welcome',
+    redirectUri: 'https://criticalpass2.azurewebsites.net/welcome',
     cacheLocation: 'sessionStorage',
     loginScopes: ['openid', 'offline_access'],
     exposedApiScope: 'https://criticalplayground.onmicrosoft.com/api/read',
-    postLogoutUrl: 'https://criticalpass.azurewebsites.net/home/',
+    postLogoutUrl: 'https://criticalpass2.azurewebsites.net/home/',
     clientID: '7515b8bc-44ba-4f60-9740-62b9ac197bf3',
     payPalClientId: 'AUyE2UNCsa6sgAKS3Ccj4WUzXw-PisRoJL2zn9pzxbN5sje0xalPOx9ioUCug9sK6HQF9Vybu2Bh_4LB',
     criticalPathApi: 'https://criticalpathapifunctions.azurewebsites.net/api/',
