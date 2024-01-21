2713c6b99c2da1d552b7cdd3cad8f82954d0eb95
Aaron Friedman <afriedman111@hotmail.com>
2024-01-21 14:21:57 -0500

    fix: injected the extended logger

--- projects/apps/critical-web/src/app/app.component.ts
+++ projects/apps/critical-web/src/app/app.component.ts
@@ -3,7 +3,6 @@ import { ExplorerLibModule } from '@critical-pass/web-lib';
 import { RouterOutlet } from '@angular/router';
 import { AppModule } from './app.module';
 import { LandingsModule } from '@critical-pass/features/landing';
-import { LoggerService } from '@critical-pass/core';
 @Component({
     selector: 'critical-pass-root',
     templateUrl: './app.component.html',
