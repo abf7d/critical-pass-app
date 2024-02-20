4ee3ab26600ec49b6be4f5ab69650eafa50f69aa
Aaron Friedman <afriedman111@hotmail.com>
2024-02-19 19:31:27 -0500

    fix: fixed timecost test with filecompiler stub

--- projects/libs/charts/src/lib/charts/time-cost/time-cost.component.cy.ts
+++ projects/libs/charts/src/lib/charts/time-cost/time-cost.component.cy.ts
@@ -2,9 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { DashboardService, DASHBOARD_TOKEN, EventService, EVENT_SERVICE_TOKEN, ZametekApiService } from '@critical-pass/shared/data-access';
 import { TimeCostComponent } from './time-cost.component';
 import { TimeCostModule } from './time-cost.module';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { Observable, of } from 'rxjs';
 import { Project } from '@critical-pass/project/types';
+import { FileCompilerService } from '../../../../../shared/project-utils/src/lib/services';
 
 describe(TimeCostComponent.name, () => {
     beforeEach(() => {
@@ -18,11 +18,12 @@ describe(TimeCostComponent.name, () => {
         };
         TestBed.overrideComponent(TimeCostComponent, {
             add: {
-                imports: [TimeCostModule, HttpClientTestingModule],
+                imports: [TimeCostModule],
                 providers: [
                     { provide: ZametekApiService, useValue: zametek },
                     { provide: DASHBOARD_TOKEN, useClass: DashboardService },
                     { provide: EVENT_SERVICE_TOKEN, useClass: EventService },
+                    { provide: FileCompilerService, useValue: {} },
                 ],
             },
         });
