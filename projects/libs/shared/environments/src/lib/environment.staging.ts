dd29d6b866e4c1fbab1a9c0298d6762b4e28f311
Aaron Friedman <afriedman111@hotmail.com>
2024-05-11 12:42:14 -0400

    fix: github-pages support

---
+++ projects/libs/shared/environments/src/lib/environment.staging.ts
@@ -0,0 +1,22 @@
+import { NgxLoggerLevel } from 'ngx-logger';
+
+export const environment = {
+    production: true,
+    clientDeugLevel: 7, // 1 is debug
+    serverDebugLevel: 7, // 7 is off
+    serverLoggerApi: 'https://<place-holder>',
+    mappingApi: 'https://criticalpathapi.azurewebsites.net/api/', // for msproj files and building arrow diagram
+    authority: 'https://criticalplayground.b2clogin.com/criticalplayground.onmicrosoft.com/B2C_1_DefaultSignInSignUp',
+    knownAuthorities: ['criticalplayground.b2clogin.com'],
+    redirectUri: 'https://abf7d.github.io/critical-pass-app/welcome',
+    cacheLocation: 'sessionStorage',
+    loginScopes: ['openid', 'offline_access'],
+    exposedApiScope: 'https://criticalplayground.onmicrosoft.com/api/read',
+    postLogoutUrl: 'https://abf7d.github.io/critical-pass-app/',
+    clientID: '7515b8bc-44ba-4f60-9740-62b9ac197bf3',
+    payPalClientId: 'AUyE2UNCsa6sgAKS3Ccj4WUzXw-PisRoJL2zn9pzxbN5sje0xalPOx9ioUCug9sK6HQF9Vybu2Bh_4LB',
+    criticalPathApi: 'https://criticalpathapifunctions.azurewebsites.net/api/',
+    jiraClientId: '1SdMM8pTryWCljI1Awm9drfKvnU2BR2H',
+    jiraClientSecret: '123',
+    logLevel: NgxLoggerLevel.ERROR,
+};
