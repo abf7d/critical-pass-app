6110a961745113956d5a840006b8b5a8cfe3aa2d
Aaron Friedman <afriedman111@hotmail.com>
2024-07-20 14:09:50 -0400

    fix: made welecome cmpnt responsive and added consent box to req-access

--- projects/libs/features/landing/src/lib/components/request-access/request-access.component.ts
+++ projects/libs/features/landing/src/lib/components/request-access/request-access.component.ts
@@ -2,15 +2,17 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MsalService } from '@critical-pass/auth';
+import { FormsModule } from '@angular/forms';
 
 @Component({
     selector: 'da-request-access',
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     templateUrl: './request-access.component.html',
     styleUrl: './request-access.component.scss',
 })
 export class RequestAccessComponent {
+    public hasConsented: boolean = false;
     constructor(private authService: MsalService) {}
     public logout() {
         this.authService.logout();
