0e62d46499fa73770890efe6a4b7ba53bfaff2f5
Aaron Friedman <afriedman111@hotmail.com>
2024-08-01 21:49:10 -0400

    fix: cleaned request-access page and updated contact-us

--- projects/libs/features/landing/src/lib/components/contact-us/contact-us.component.ts
+++ projects/libs/features/landing/src/lib/components/contact-us/contact-us.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { EmailFormComponent } from '../../../../../../shared/layout/src/lib/email-form/email-form.component';
 
 @Component({
     selector: 'da-contact-us',
     standalone: true,
-    imports: [CommonModule, FormsModule],
+    imports: [CommonModule, FormsModule, EmailFormComponent],
     templateUrl: './contact-us.component.html',
     styleUrl: './contact-us.component.scss',
 })
