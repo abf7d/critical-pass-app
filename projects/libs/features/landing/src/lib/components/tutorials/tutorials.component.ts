66e7e0efcb32d24f0f66968aa6d01374c400f13e
Aaron Friedman <afriedman111@hotmail.com>
2024-08-04 16:53:59 -0400

    fix: fixed assgnRsrcs edit grid calandar btns, added assgnRcrcs video

--- projects/libs/features/landing/src/lib/components/tutorials/tutorials.component.ts
+++ projects/libs/features/landing/src/lib/components/tutorials/tutorials.component.ts
@@ -15,6 +15,9 @@ export class TutorialsComponent {
         { menuName: 'Import Data', type: 'uploadExcelFile', id: 'tRR8aRHb4L4', videoTitle: 'Import Data into Project', si: 'LPBhkhIOGu3Vu_JP' },
         { menuName: 'Quick Start Example', type: 'quickStart', id: 'T0jwmYAB8gA', videoTitle: 'Quick Start Example', si: 'CdoTCesic5FWzQr6' },
         { menuName: 'Application Layout', type: 'applicationLayout', id: 'R3kPmTzawCE', videoTitle: 'Application Layout', si: 'HmDHijxNQPRuTAhI' },
+        { menuName: 'Scenario Planning', type: 'scenarioPlanning', id: '5xBKlB1LBSE', videoTitle: 'Scenario Planning', si: 'VKlOaFgVXNYpxgPC' },
+        { menuName: 'Network Analysis', type: 'networkAnalysis', id: 'MRqlK14DbbA', videoTitle: 'Network Analysis', si: 'b0n7JxNhK5a7wtHa' },
+        { menuName: 'Resource Assignment', type: 'resourceAssignment', id: 'PoMuLcZ9xkk', videoTitle: 'Resource Assignment', si: 'hRwEHp10cJeQbaSW' },
     ];
     public constructor(private sanitizer: DomSanitizer) {
         this.videos.forEach((video: Video) => {
