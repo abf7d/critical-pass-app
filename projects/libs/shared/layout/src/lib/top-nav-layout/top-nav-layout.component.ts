ca5ba1cd49dd1a43c0a675964a5d1b34a66eac8d
Aaron <afriedman111@gmail.com>
2024-10-26 18:26:44 -0400

    fix: made top-nav sticky

--- projects/libs/shared/layout/src/lib/top-nav-layout/top-nav-layout.component.ts
+++ projects/libs/shared/layout/src/lib/top-nav-layout/top-nav-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { environment } from '@critical-pass/shared/environments';
@@ -10,6 +10,7 @@ import { environment } from '@critical-pass/shared/environments';
     styleUrl: './top-nav-layout.component.scss',
 })
 export class TopNavLayoutComponent {
+    @Input() isSticky: boolean = true;
     constructor(private router: Router) {}
     public navigateHome(): void {
         window.location.href = environment.postLogoutUrl;
